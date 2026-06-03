de93e483803543933d36cd72da57cd184b5edc81
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-03 22:21:38 +0530

    feat: complete multi-layered config parsing and fallback logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+
 use serde::{Deserialize, Serialize};
 use clap:: {Parser,Subcommand};
+use std::fs::File;
+use std::io::Read;
 
 
 
@@ -33,4 +36,42 @@ struct CLI {
 #[derive(Subcommand,Debug)]
 enum Commands {
     Price,
+}
+
+#[tokio::main]
+
+async fn main() {
+   let cli = CLI::parse(); 
+
+   let mut currency = cli.currency.clone();
+
+   if currency.is_none() {
+    println!("No currency flag provided. Checking crypto_config.json...");
+
+    if let Ok(mut file) = File::open("crypto_config.json") {
+        let mut contents = String::new();
+
+        if file.read_to_string(& mut contents).is_ok() {
+            if let Ok(parsed_config) = serde_json::from_str::<Config>(&contents) {
+               println!("Successfully loaded currency from config file!") ;
+               currency = Some(parsed_config.default_currency)
+            }
+        }
+    }
+   }
+
+   let new_currency = currency.unwrap_or_else(|| "usd".to_string());
+   println!("🎯 Final selected currency to look up: {}", new_currency);
+
+   match &cli.subcommand {
+    Some(Commands::Price) => {
+        println!("Price subcommand detected! Ready to fetch data...");
+
+    }
+    None => {
+        println!("Please specify a subcommand, e.g., cargo run -- price");
+    }
+   }
+
+
 }
